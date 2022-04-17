5b2e81830f2b0b8e354e27b548c7a73e712ae01f
Mohammedasif Pinitod <87132170+MohammedasifP@users.noreply.github.com>
2022-04-17 23:06:21 +0530

    done

--- src/Components/Teacher_details/Teacher_details.jsx
+++ src/Components/Teacher_details/Teacher_details.jsx
@@ -1,11 +1,31 @@
+import { useState,useEffect } from "react";
 import { useParams } from "react-router-dom"
-
+import './Classdetails.css'
 export const Teaccherdetails=()=>{
+const [data,setData]=useState([])    
 const {id}=useParams();
-
+useEffect(()=>{
+    fetch(`https://asifbackendcode.herokuapp.com/class`).then(Response=>Response.json()).then(data=>{setData(data)}) 
+},[])
+const displaydata=data.filter((elem)=>{
+    return id==elem.teacher_id._id
+})   
+console.log(displaydata)
     return(
         <div>
-            hi
+           {
+               displaydata.map((elem)=>{
+                   return (<div className="details">
+                         <p>Name:{elem.teacher_id.name}</p>
+                         <p>Age:{elem.teacher_id.age}</p>
+                         <p>Gender:{elem.teacher_id.gender}</p>
+                         <p>Grade:{elem.grade}</p>
+                         <p>Section:{elem.section}</p>
+                         <p>Subjects:{elem.subject}</p>
+
+                         </div>)
+               })
+           }   
         </div>
     )
 } 
